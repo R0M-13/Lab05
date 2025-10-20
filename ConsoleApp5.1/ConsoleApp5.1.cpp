ad68697f73853235403604bf2b87f908d2cf45a4
R0M-13 <rompro224@gmail.com>
2025-10-20 19:39:46 +0300

    first master commit5

---
+++ ConsoleApp5.1/ConsoleApp5.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double g(double a, double b) {
+	return sin(a * b) / (a * a + b * b);
+}
+
+int main() {
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+
+	double numerator = g(pow(s, 2), t + 1) + g(pow(t, 2), s + 1);
+	double denominator = 1 + pow(g(s + t, s * t), 2);
+	double result = numerator / denominator;
+
+	cout << "result = " << result << endl;
+	return 0;
+}
